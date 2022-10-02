f436507284c0e8f7db16a331b86bcc80d0004c41
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-10-02 12:40:44 -0300

    fix: eslint fixing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const { PORT , UPSIDEDOWN_MODE } = process.env;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
 const app = express();
 
